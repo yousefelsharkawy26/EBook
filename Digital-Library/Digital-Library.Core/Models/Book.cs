bccd10592d5cce44920d9ac3ae85ad6b6612283a
Mahmoud Mohamed Abd ElRaouf <mm4394405@gmail.com>
2025-09-03 12:57:03 +0300

    Add [Borrowing Class] - update book class

---
+++ Digital-Library/Digital-Library.Core/Models/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Core.Models
+{
+    internal class Book
+    {
+        [Key]
+        public Guid BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double PricePhysical { get; set; }
+        public double PricePDFPerDay { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+        public bool HasPDF { get; set; }
+        public bool isBorrowable { get; set; }
+        public string PDF_FilePath { get; set; }
+        public DateTime UploadDate { get; set; }
+
+        //public Category Category { get; set; }
+        //[ForeignKey(nameof(Category))]
+        public Guid CategoryID { get; set; }
+
+        public ICollection<Borrowing> Borrowing { get; set; }
+
+
+        /*public Vendor Vendor { get; set; }
+        [ForeignKey(nameof(Vendor))]
+        public Guid VendorID { get; set; }*/
+
+        //public ICollection<OrderDetail> OrderDetails { get; set; }
+        //public ICollection<CartDetail> CartDetails { get; set; }
+
+
+    }
+}
