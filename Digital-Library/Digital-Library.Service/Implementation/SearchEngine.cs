bd35b8a77e7f80e53fe780787daf43a34f74beac
Mahmoud Mohamed Abd ElRaouf <mm4394405@gmail.com>
2025-09-03 20:59:49 +0300

    Add Search Engine Service and implementation

---
+++ Digital-Library/Digital-Library.Service/Implementation/SearchEngine.cs
@@ -0,0 +1,27 @@
+﻿using Digital_Library.Core.Models;
+using Digital_Library.Infrastructure.UnitOfWork.Interface;
+using Digital_Library.Service.Interface;
+
+namespace Digital_Library.Service.Implementation
+{
+    public class SearchEngine : ISearchEngine
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public SearchEngine(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public Task<IEnumerable<Book>> SearchBooksAsync(string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+                return (Task<IEnumerable<Book>>)Enumerable.Empty<Book>();
+
+            searchTerm = searchTerm.ToLower();
+
+            return _unitOfWork.Books.
+                GetManyAsync(b => b.Title.ToLower().Contains(searchTerm)
+                || b.Author.ToLower().Contains(searchTerm)
+                , b => b.Category , b => b.Vendor);
+        }
+    }
+}
---
+++ Digital-Library/Digital-Library.Service/Interface/ISearchEngine.cs
@@ -0,0 +1,14 @@
+﻿using Digital_Library.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Service.Interface
+{
+    public interface ISearchEngine
+    {
+        public Task<IEnumerable<Book>> SearchBooksAsync(string searchTerm);
+    }
+}
