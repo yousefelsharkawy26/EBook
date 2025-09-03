ffab2b0cc16e45916f70c3ae5f7ae941bea4a79b
makram-sys <mzhar711@gmail.com>
2025-09-03 14:36:20 +0300

    Update IVendor service name

---
+++ Digital-Library/Digital-Library.Service/Interface/IVendorService.cs
@@ -0,0 +1,12 @@
+﻿using Digital_Library.Core.Models;
+
+namespace Digital_Library.Service.Interface;
+public interface IVendorService
+{
+    Task<IEnumerable<Vendor>> GetAllVendorsAsync();
+    Task<Vendor> GetVendorByIdAsync(int id);
+    Task<Vendor> AddVendorAsync(Vendor vendor);
+    Task<Vendor> UpdateVendorAsync(int id, Vendor vendor);
+    Task<bool> DeleteVendorAsync(int id);
+}
+
