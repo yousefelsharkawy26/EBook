2b2fd1e7f3b3c93b1c7f0bd4e80098a557a303c7
OmarEL-Deeb <omar_eldeeb@gmail.com>
2025-09-03 13:11:30 +0300

    Cart&CartDetail

---
+++ Digital-Library/Digital-Library.Core/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Core.Models
+{
+    public class Cart
+    {
+        public Guid CartId { get; set; }
+        public DateTime CreatedDate;
+    }
+}
