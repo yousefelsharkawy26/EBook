e5e7d23b7834be1d639db2615a22a97d598990be
Mahmoud Mohamed Abd ElRaouf <mm4394405@gmail.com>
2025-09-03 14:46:27 +0300

    Add UserService

---
+++ Digital-Library/Digital-Library.Service/Implementation/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Digital_Library.Core.Models;
+using Digital_Library.Service.Interface;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Service.Implementation
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<User> _userManager;
+        public UserService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public Task<User> ChangeAddress(Guid id, string Address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> ChangeName(Guid id, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> ChangePhoneNumber(Guid id, string PhoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Digital-Library/Digital-Library.Service/Interface/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Digital_Library.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Service.Interface
+{
+    public interface IUserService
+    {
+        Task<User> ChangeName(Guid id , string name);
+
+        Task<User> ChangeAddress(Guid id , string Address);
+
+        Task<User> ChangePhoneNumber(Guid id, string PhoneNumber);
+    }
+}
