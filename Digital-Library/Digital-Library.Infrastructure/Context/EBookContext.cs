d9dbb0d32a101bcda632675e7083ce593c0b6c5d
yousefelsharkawy26 <yousefelsharkawy@std.mans.edu.eg>
2025-09-03 15:01:14 +0300

    Update Context & Add IAuthService

--- Digital-Library/Digital-Library.Infrastructure/Context/EBookContext.cs
+++ Digital-Library/Digital-Library.Infrastructure/Context/EBookContext.cs
@@ -1,15 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Digital_Library.Core.Models;
 
 namespace Digital_Library.Infrastructure.Context
 {
     public class EBookContext: IdentityDbContext
     {
         public EBookContext(DbContextOptions options)
-            : base(options)
-        {
-            
-        }
+            : base(options) { }
 
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Borrowing> Borrowings { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartDetail> CartsDetails { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
     }
 }
