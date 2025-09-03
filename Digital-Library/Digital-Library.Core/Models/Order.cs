d6fb5e8d5789218bf8ea378e3a6dcca378428348
sasa6776 <moustafa.hesham67@gmail.com>
2025-09-03 14:30:58 +0300

    Add order & orederDetails

---
+++ Digital-Library/Digital-Library.Core/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using Digital_Library.Core.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Core.Models
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public Status Status { get; set; }
+
+    }
+}
