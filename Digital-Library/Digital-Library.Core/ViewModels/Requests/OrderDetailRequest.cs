c191632c4cf71ab93b01398122b9dee3a4390e72
Yousef Hany <Joo.gmail.com>
2025-09-04 15:59:07 +0300

    Finish Order Service

--- Digital-Library/Digital-Library.Core/ViewModels/Requests/OrderDetailRequest.cs
+++ Digital-Library/Digital-Library.Core/ViewModels/Requests/OrderDetailRequest.cs
@@ -2,29 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Digital_Library.Core.Models
+namespace Digital_Library.Core.ViewModels.Requests
 {
-	public class OrderDetail
+	public class OrderDetailRequest
 	{
-		[Key]
-		public string Id { get; set; }=Guid.NewGuid().ToString();
 		[Required]
 		public FormatType FormatType { get; set; }
 		[Required]
 		public decimal Price { get; set; }
 		[Required]
 		public int Quantity { get; set; }
-		public Order? Order { get; set; }
-		[ForeignKey(nameof(Order))]
-		[Required]
-		public string OrderId { get; set; }
-		public Book? Book { get; set; }
-		[ForeignKey(nameof(Book))]
 		[Required]
 		public string BookId { get; set; }
 	}
