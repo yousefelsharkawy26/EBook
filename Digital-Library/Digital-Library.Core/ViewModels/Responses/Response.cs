555fb03e6888271f600a34be7d4f4ddbd1932c18
Yousef Hany <Joo.gmail.com>
2025-09-04 10:17:50 +0300

    Update Respone

--- Digital-Library/Digital-Library.Core/ViewModels/Responses/Response.cs
+++ Digital-Library/Digital-Library.Core/ViewModels/Responses/Response.cs
@@ -8,9 +8,15 @@ namespace Digital_Library.Core.ViewModels.Responses
 {
 	public class Response
 	{
-		public bool IsSuccess { get; set; }
-		public string? Message { get; set; }
+		public bool Success { get; set; }
+		public string Message { get; set; }
 		public object? Data { get; set; }
 
+		public static Response Ok(string message, object? data = null) =>
+						new Response { Success = true, Message = message, Data = data };
+
+		public static Response Fail(string message) =>
+						new Response { Success = false, Message = message };
 	}
+
 }
