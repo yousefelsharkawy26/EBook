89d497a5df720a269379283f2416195fc00aa1cb
yousefelsharkawy26 <yousefelsharkawy@std.mans.edu.eg>
2025-09-06 13:04:26 +0300

    Update Layout

---
+++ Digital-Library/Digital-Library/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Digital_Library.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Profile()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) 
+                return NotFound();
+
+            return View(user);
+        }
+
+
+        public async Task<IActionResult> Edit()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+
+    }
+}
