6102f74772da332e143a81bc6fd89dbd70cb7606
Yousef Hany <Joo.gmail.com>
2025-09-06 11:30:17 +0300

    Finish Cart Controller

---
+++ Digital-Library/Digital-Library/Controllers/CartController.cs
@@ -0,0 +1,72 @@
+﻿using Digital_Library.Core.Models;
+using Digital_Library.Core.Enums;
+using Digital_Library.Core.ViewModels.Requests;
+using Digital_Library.Service.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Controllers
+{
+	[Route("Cart")]
+	[Authorize]
+	public class CartController : Controller
+	{
+		private readonly ICartService cartService;
+
+		public CartController(ICartService cartService)
+		{
+			this.cartService = cartService;
+		}
+
+		[HttpGet("Index")]
+		public async Task<IActionResult> Index()
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (userId == null) return Unauthorized();
+
+			var cartItems = await cartService.GetCartAsync(userId);
+			return View(cartItems.Data as Cart);
+		}
+
+		[HttpPost("AddItem")]
+		public async Task<IActionResult> AddItem([FromBody] CartDetailRequest request)
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (userId == null) return Unauthorized();
+
+			var result = await cartService.AddItemAsync(userId, request);
+			return Json(result);
+		}
+
+		[HttpGet("Remove")]
+		public async Task<IActionResult> Remove(string cartDetailId)
+		{
+			if (string.IsNullOrEmpty(cartDetailId))
+				return BadRequest("Invalid item id");
+
+			var result = await cartService.RemoveItemAsync(cartDetailId);
+			return RedirectToAction(nameof(Index));
+		}
+
+		[HttpPost("UpdateQuantity")]
+		public async Task<IActionResult> UpdateQuantity([FromForm] string cartDetailId, [FromForm] int quantity)
+		{
+			if (string.IsNullOrEmpty(cartDetailId))
+				return BadRequest("Invalid item id");
+			var result = await cartService.UpdateCartAsync(cartDetailId, quantity);
+			return Json(result);
+		}
+
+		[HttpPost("Clear")]
+		public async Task<IActionResult> Clear()
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (userId == null) return Unauthorized();
+
+			var result = await cartService.ClearCartAsync(userId);
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
