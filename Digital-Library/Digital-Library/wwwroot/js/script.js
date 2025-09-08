cfd69b4986133c534e13f5b8b6cdd40d2179b93b
yousefelsharkawy26 <yousefelsharkawy@std.mans.edu.eg>
2025-09-08 21:00:51 +0300

    Solve front errors

--- Digital-Library/Digital-Library/wwwroot/js/script.js
+++ Digital-Library/Digital-Library/wwwroot/js/script.js
@@ -169,9 +169,4 @@
 
     }); // End of a document ready
 
-    window.addEventListener("load", function () {
-      const preloader = document.getElementById("preloader");
-      preloader.classList.add("hide-preloader");
-    });
-
 })(jQuery);
