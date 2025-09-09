26bb8be2db2dc40c6d555514fcbf0859d5a61e1f
yousefelsharkawy26 <yousefelsharkawy@std.mans.edu.eg>
2025-09-09 14:20:12 +0300

    Running Contact Us Messages

--- Digital-Library/Digital-Library/Controllers/HomeController.cs
+++ Digital-Library/Digital-Library/Controllers/HomeController.cs
@@ -1,8 +1,17 @@
+using Digital_Library.Core.Models;
+using Digital_Library.Core.ViewModels;
 using Digital_Library.Core.ViewModels.Requests;
 using Digital_Library.Models;
 using Digital_Library.Service.Interface;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using NuGet.Common;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Digital_Library.Controllers
 {
@@ -10,11 +19,24 @@ namespace Digital_Library.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ICategoryService _categoryService;
+        private readonly IEmailSender _emailSender;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IUrlHelperFactory _urlHelperFactory;
+        private readonly IActionContextAccessor _actionContextAccessor;
 
-        public HomeController(ILogger<HomeController> logger , ICategoryService categoryService)
+        public HomeController(ILogger<HomeController> logger,
+                              ICategoryService categoryService,
+                              IEmailSender emailSender,
+                              IWebHostEnvironment webHostEnvironment,
+                              IUrlHelperFactory urlHelperFactory,
+                              IActionContextAccessor actionContextAccessor)
         {
             _logger = logger;
             _categoryService = categoryService;
+            _emailSender = emailSender;
+            _webHostEnvironment = webHostEnvironment;
+            _urlHelperFactory = urlHelperFactory;
+            _actionContextAccessor = actionContextAccessor;
         }
 
         public async Task<IActionResult> Index()
@@ -34,6 +56,39 @@ namespace Digital_Library.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Contact(ContactViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string templatePath = Path.Combine(_webHostEnvironment.WebRootPath, "html/ContactConfirmation.html");
+                string userBody = await System.IO.File.ReadAllTextAsync(templatePath);
+                var actionContext = _actionContextAccessor.ActionContext;
+                var urlHelper = _urlHelperFactory.GetUrlHelper(actionContext);
+
+                var siteLink = urlHelper.Action("Index", "Home", null, actionContext.HttpContext.Request.Scheme);
+
+                userBody = userBody.Replace("[User's Name]", model.Name)
+                                   .Replace("[App Name]", "E-BOOK")
+                                   .Replace("[Your Website URL]", siteLink)
+                                   .Replace("[Company Name]", "ITI")
+                                   .Replace("[Company Address]", "Mansoura University");
+
+                try
+                {
+                    await _emailSender.SendEmailAsync(model.Email, model.Subject, userBody);
+                    _logger.LogInformation($"Message Send Successfully From {nameof(Contact)}");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message, $"in {nameof(Contact)}");
+                    return View(model);
+                }
+            }
+            return View();
+
+        }
         public IActionResult Privacy()
         {
             return View();
